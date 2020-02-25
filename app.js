9c314d0baa0e086af3618fb10bb34b6f45fd7110
neil10july <neil10july@hotmail.com>
2020-02-25 18:51:48 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const api_router = require("./routes/api.router");
 const {
   send404,
@@ -8,6 +9,7 @@ const {
   psqlErrorHandler
 } = require("./errors/error.handlers");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", api_router);
 app.use("/*", send404);
